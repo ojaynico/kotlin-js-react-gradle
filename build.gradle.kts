f7df9cdff2eadfc78bc61c3b7ed568558c5b9139
ojaynico <ojaynico@gmail.com>
2020-01-21 23:47:31 +0300

    Updated gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,19 +23,22 @@ dependencies {
 }
 
 kotlin {
-    sourceSets["main"].dependencies {
-        implementation(kotlin("stdlib-js"))
-        implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.6.12")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.3")
-        implementation(npm("@jetbrains/kotlin-react", "16.9.0-pre.89"))
-        implementation(npm("@jetbrains/kotlin-react-dom", "16.9.0-pre.89"))
-        implementation(npm("@jetbrains/kotlin-react-router-dom", "4.3.1-pre.89"))
-        implementation(npm("@jetbrains/kotlin-extensions", "1.0.1-pre.89"))
-        implementation(npm("react", "16.12.0"))
-        implementation(npm("react-dom", "16.12.0"))
-        implementation(npm("react-router-dom", "5.1.2"))
+    sourceSets {
+        main {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
+                implementation("org.jetbrains.kotlinx:kotlinx-html-js:0.6.12")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.3")
+                implementation(npm("@jetbrains/kotlin-react", "16.9.0-pre.89"))
+                implementation(npm("@jetbrains/kotlin-react-dom", "16.9.0-pre.89"))
+                implementation(npm("@jetbrains/kotlin-react-router-dom", "4.3.1-pre.89"))
+                implementation(npm("@jetbrains/kotlin-extensions", "1.0.1-pre.89"))
+                implementation(npm("react", "16.12.0"))
+                implementation(npm("react-dom", "16.12.0"))
+                implementation(npm("react-router-dom", "5.1.2"))
+            }
+        }
     }
-
 }
 
 kotlin.target.browser { }
